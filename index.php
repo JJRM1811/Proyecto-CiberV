81325d39bc17d3c57bacc8846542dfc97a3938a7
JJRM1811 <jassir182004@gmail.com>
2025-12-10 09:59:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 // VULNERABILIDAD 1: Credenciales en duro (Hardcoded Credentials)
-$username = "admin";
+$username = "admin123";
 $password = "12345"; // SonarQube odia esto
 
 $user_input = $_GET['user'];
 
 // VULNERABILIDAD 2: Cross-Site Scripting (XSS)
 // Imprimir directamente lo que escribe el usuario sin sanitizar
-echo "Bienvenido PLP " . $user_input; 
+echo "Bienvenido hola " . $user_input; 
 
 if ($_POST['pass'] == $password) {
     echo "Login correcto";
